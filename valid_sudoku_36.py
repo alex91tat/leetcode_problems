1534134001ac61ec3213e3b208c5971c2a0406e5
alex91tat <tatalexandru91@gmail.com>
2025-11-30 13:23:28 +0200

    probelm 36 done

---
+++ valid_sudoku_36.py
@@ -0,0 +1,45 @@
+# 36. Valid Sudoku
+# Problem link: https://leetcode.com/problems/valid-sudoku/description/
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # for rows
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                val = board[i][j]
+
+                if val in s:
+                    return False
+                elif val != '.':
+                    s.add(val)
+
+        # for columns
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                val = board[j][i]
+
+                if val in s:
+                    return False
+                elif val != '.':
+                    s.add(val)
+
+        # for sub boxes
+        start_pos = [(0, 0), (0, 3), (0, 6),
+                     (3, 0), (3, 3), (3, 6),
+                     (6, 0), (6, 3), (6, 6),]
+
+        for i, j in start_pos:
+            s = set()
+            for row in range(i, i + 3):
+                for col in range(j, j + 3):
+                    val = board[row][col]
+
+                    if val in s:
+                        return False
+                    elif val != '.':
+                        s.add(val)
+
+        return True
