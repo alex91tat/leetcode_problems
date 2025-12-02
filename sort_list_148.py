559ed7433bb35176bca41175408e03922b5ee938
alex91tat <tatalexandru91@gmail.com>
2025-12-02 11:28:34 +0200

    problem 148 done

---
+++ sort_list_148.py
@@ -0,0 +1,45 @@
+# 148. Sort List
+# Problem link: https://leetcode.com/problems/sort-list/description/
+from typing import Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        mid = self.getMid(head)
+        right = mid.next
+        mid.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def merge(self, left, right):
+        tail = d = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+
+        tail.next = left if left else right
+
+        return d.next
